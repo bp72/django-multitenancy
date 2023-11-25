e3ef1c72e5a563bb28c857c50c6058fc14f85a13
Pavel Bitiukov <pavleg.bityukov@gmail.com>
2023-11-25 22:30:08 +0000

    [feature] Add tox

--- testdjangoproject/tenants/apps.py
+++ testdjangoproject/tenants/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TenantsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tenants'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "tenants"
